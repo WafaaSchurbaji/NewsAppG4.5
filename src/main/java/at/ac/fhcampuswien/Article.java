74258344fe86a7e7db26d06d2f25551213cdccb6
Anita <Anita.wiesinger@stud.fh-campuswien.ac.at>
2022-03-02 12:25:32 +0100

    Creation of Class Article

--- src/main/java/at/ac/fhcampuswien/Article.java
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -1,4 +1,24 @@
 package at.ac.fhcampuswien;
 
 public class Article {
+    private String author;
+    private String title;
+
+    // Constructor
+    public Article(String author, String title){
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString(){
+        return "String";
+    }
 }
