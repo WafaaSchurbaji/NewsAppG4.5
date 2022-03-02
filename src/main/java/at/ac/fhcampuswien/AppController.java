ccce6cd913b0619ec1947e727fb7836bd6471ef0
Wafaatalabani1994 <wafaa.talabani@stud.fh-campuswien.ac.at>
2022-03-02 11:16:25 +0100

    Class created

---
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -0,0 +1,17 @@
+package at.ac.fhcampuswien;
+
+import java.util.List;
+
+public class AppController {
+    private List<Article> articles;
+    public AppController(){
+
+    }
+    public void setArticles(List<Article> articles){
+
+    }
+    public int getArticleCount(){
+        return 0 ;
+    }
+
+}
---
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -0,0 +1,9 @@
+package at.ac.fhcampuswien;
+
+public class Menu {
+    private AppController controller;
+    private static final String INVALID_USER_INPUT_MESSAGE = "Invalid Input";
+    private static final String EXIT_MESSAGE="finished";
+
+
+}
