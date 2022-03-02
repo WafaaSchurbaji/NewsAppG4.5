88fd370ba092ef1d4ea50bafab7d1253c810e175
Temu-Nduwayezu <glory.temu-nduwayezu@stud.fh-campuswien.ac.at>
2022-03-02 12:40:54 +0100

    Menu method

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -1,9 +1,32 @@
 package at.ac.fhcampuswien;
 
-public class Menu {
+import java.util.List;
+
+public class Menu  {
     private AppController controller;
     private static final String INVALID_USER_INPUT_MESSAGE = "Invalid Input";
     private static final String EXIT_MESSAGE="finished";
 
+    public void  start(){}
 
+    private void handleInput(String input){}
+
+
+    private void getArticleCount(AppController ctrl) {
+
+    }
+
+    private void getTopHeadlinesAustria(AppController ctrl) {
+
+    }
+    private void getAllNewsBitcoin(AppController ctrl){}
+
+    private void printExitMessage(){}
+
+    private void printInvalidInputMessage(){}
+    private void printMenu(){}
 }
+
+
+
+
