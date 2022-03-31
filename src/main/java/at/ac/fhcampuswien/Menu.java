2497c31c2fce8a1a49a1b2df41e90e45310b2cec
Temu-Nduwayezu <glory.temu-nduwayezu@stud.fh-campuswien.ac.at>
2022-03-31 17:41:28 +0200

    Menu almost done

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -7,36 +7,63 @@ import java.util.Scanner;
 public class Menu  {
     private AppController controller;
     private static final String INVALID_USER_INPUT_MESSAGE = "Invalid Input";
-    private static final String EXIT_MESSAGE="finished";
+    private static final String EXIT_MESSAGE="Bye Bye";
 
 
     public  void  start(){
+        printMenu();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim().toLowerCase();
 
+        handleInput(input);
+            }
 
-    }
 
     private void handleInput(String input){
+      /* if (!(input == "a" || input == "b" || input == "y" || input == "q"))
+       {
+          // printInvalidInputMessage();
+        }*/
+        if ("a".equals(input)) {
+            getTopHeadlinesAustria(controller);
+        } else if ("b".equals(input)) {
+            getAllNewsBitcoin(controller);
+        } else if ("y".equals(input)) {
+            getArticleCount(controller);
+        } else if ("q".equals(input)) {
+            printExitMessage();
+        } else {
+            printInvalidInputMessage();
+        }
     }
 
-
     private void getArticleCount(AppController ctrl) {
-        this.controller.getArticleCount();
+
+        System.out.println(this.controller.getArticleCount());
     }
 
     private void getTopHeadlinesAustria(AppController ctrl) {
+        controller = new AppController();
+        System.out.println( controller.getTopHeadlinesAustria());
+
 
     }
     private void getAllNewsBitcoin(AppController ctrl) {
+        controller= new AppController();
+        System.out.println(controller.getAllNewsBitcoin());
 
     }
 
     private static void printExitMessage(){
-        System.out.println("Bye Bye");
         System.out.println(EXIT_MESSAGE);
+        System.exit(0);
     }
 
     private static void printInvalidInputMessage(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println(INVALID_USER_INPUT_MESSAGE);
+        System.out.println("Please enter what you wanna do:");
+       scanner.nextLine().trim().toLowerCase();
     }
     private static void printMenu() {
         Scanner scanner = new Scanner(System.in);
@@ -46,6 +73,7 @@ public class Menu  {
         System.out.println("Enter what you wanna do:");
         System.out.println("a: Get top headlines austria \nb: Get all news about bitcoin" +
                 "\ny: Count articles \nq: Quit program");
+        System.out.println();
     }
 }
 
