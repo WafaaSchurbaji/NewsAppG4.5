7b0a3ddffec74e0e4dd747d87c8cffa30e6e1a8f
Temu-Nduwayezu <glory.temu-nduwayezu@stud.fh-campuswien.ac.at>
2022-04-20 14:42:56 +0200

    NewsResponse

--- src/main/java/at/ac/fhcampuswien/api/NewsResponse.java
+++ src/main/java/at/ac/fhcampuswien/api/NewsResponse.java
@@ -1,5 +1,23 @@
 package at.ac.fhcampuswien.api;
 
-public class NewsResponse {
+import at.ac.fhcampuswien.Article;
 
+import java.util.List;
+
+public class NewsResponse {
+    private String status;
+    private int totalResults;
+    private List<Article> articles;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
 }
