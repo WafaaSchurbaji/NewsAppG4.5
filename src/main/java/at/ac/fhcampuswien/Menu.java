346f7184cb51778efc76c000b89a12f4e88dc192
Wafaatalabani1994 <wafaa.talabani@stud.fh-campuswien.ac.at>
2022-04-30 16:24:45 +0200

    Menü veränderung

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -1,90 +1,100 @@
 package at.ac.fhcampuswien;
-import at.ac.fhcampuswien.api.NewsResponse;
-import at.ac.fhcampuswien.entity.Article;
-import at.ac.fhcampuswien.properties.SortBy;
 
+import at.ac.fhcampuswien.api.NewsResponse;
+import at.ac.fhcampuswien.controller.AppController;
+import at.ac.fhcampuswien.entity.Article;
+import at.ac.fhcampuswien.properties.Country;
+import at.ac.fhcampuswien.properties.Language;
+import at.ac.fhcampuswien.properties.SortBy;
 
 import java.util.Scanner;
 
-public class Menu  {
-    private AppController controller = new AppController();
-    private static final String INVALID_USER_INPUT_MESSAGE = "Invalid Input";
-    private static final String EXIT_MESSAGE="Bye Bye";
+public class Menu {
+    private final AppController controller = new AppController();
+    private static final String INVALID_USER_INPUT_MESSAGE = "\nInvalid Input\n";
+    private static final String EXIT_MESSAGE = "Bye Bye";
+    private Scanner scanner = new Scanner(System.in);
+
+    //getTopHeadlinesAustria
+    //getAllNewsBitcoin
+    //getNews
 
 
-    Scanner scanner = new Scanner(System.in);
-
-
-    public  void  start(){
+    public void start() {
         printMenu();
         String input = scanner.next().trim().toLowerCase();
         handleInput(input);
+    }
 
-            }
 
-
-    private void handleInput(String input){
-
+    private void handleInput(String input) {
         if ("a".equals(input)) {
-            getTopHeadlinesAustria(controller);
-           start();
+            getTopHeadlines();
         } else if ("b".equals(input)) {
-            getAllNewsBitcoin(controller);
-           start();
-        } else if ("y".equals(input)) {
-            getArticleCount(controller);
-            start();
+            getAllNewsBitcoin();
+        } else if ("c".equals(input)) {
+            getNewsByTopic();
         } else if ("q".equals(input)) {
             printExitMessage();
-
         } else {
             printInvalidInputMessage();
-            start();
-
         }
+        start();
     }
 
-    private void getArticleCount(AppController ctrl) {
-        System.out.println(ctrl.getArticleCount());
-    }
-
-    private void getTopHeadlinesAustria(AppController ctrl) {
-        System.out.println( ctrl.getTopHeadlinesAustria());
-
-
-    }
-    private void getAllNewsBitcoin(AppController ctrl) {
-        System.out.println(ctrl.getAllNewsBitcoin());
+    private void getTopHeadlines() {
 
     }
 
-    private static void printExitMessage(){
-        System.out.println(EXIT_MESSAGE);
-        System.exit(0);
-    }
-
-    private static void printInvalidInputMessage(){
-        System.out.println(INVALID_USER_INPUT_MESSAGE);
+    private void printCategoryOptions() {
 
     }
+
+    private Country getCountryPreference() {
+        System.out.println("\nChoose from the following Countries to get its headlines");
+        System.out.println("*****************************************************");
+        try {
+            for (Country country : Country.values()) {
+                System.out.println(country.getPrettyName());
+            }
+            scanner = new Scanner(System.in);
+            String country = scanner.next().trim();
+            if ("default".equalsIgnoreCase(country))
+                return Country.DEFAULT;
+            return Country.getCountryByPrettyName(country);
+        } catch (Exception exception) {
+            printInvalidInputMessage();
+            getCountryPreference();
+        }
+        return Country.DEFAULT;
+    }
+
 
     private void printResponse(NewsResponse response) {
         System.out.println("\nResponse Status: " + response.getStatus());
         System.out.println("Articles count: " + response.getTotalResults());
-        System.out.println("**\n\n");
+        System.out.println("***********************************************************************************************************************\n\n");
         for (Article article : response.getArticles()) {
             System.out.println(article.toString());
-            System.out.println("**\n\n");
+            System.out.println("***********************************************************************************************************************\n\n");
         }
+    }
+
+
+    private void getAllNewsBitcoin() {
+        Language language = getLanguagePreference();
+        SortBy sortBy = getSortingPreference();
+        NewsResponse response = controller.getAllNewsBitcoin(language, sortBy);
+        printResponse(response);
     }
 
     private SortBy getSortingPreference() {
         System.out.println("\nChoose from the following to sort by it:");
-        System.out.println("**");
+        System.out.println("******************************************");
         for (SortBy sortBy : SortBy.values()) {
             System.out.println(sortBy.name().toLowerCase());
         }
-        System.out.println("**\n");
+        System.out.println("******************************************\n");
         try {
             scanner = new Scanner(System.in);
             return SortBy.valueOf(scanner.next().trim().toUpperCase());
@@ -95,6 +105,36 @@ public class Menu  {
         return SortBy.DEFAULT;
     }
 
+    /**
+     * enum elements in a stream array
+     *
+     * @return
+     */
+
+    private Language getLanguagePreference() {
+        return Language.DEFAULT;
+    }
+
+    private void getNewsByTopic() {
+        scanner = new Scanner(System.in);
+        System.out.println("\nEnter your desired topic:");
+        System.out.println("******************************************");
+        String topic = scanner.next().trim();
+        Language language = getLanguagePreference();
+        SortBy sortBy = getSortingPreference();
+        NewsResponse response = controller.getNews(topic, language, sortBy);
+        printResponse(response);
+    }
+
+    private static void printExitMessage() {
+        System.out.println(EXIT_MESSAGE);
+        System.exit(0);
+    }
+
+    private static void printInvalidInputMessage() {
+        System.out.println(INVALID_USER_INPUT_MESSAGE);
+
+    }
 
     private static void printMenu() {
         System.out.println("""
@@ -107,8 +147,6 @@ public class Menu  {
                 c: Get news by topic\s
                 q: Quit program""");
     }
-
-
 
 }
 
